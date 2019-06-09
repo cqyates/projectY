83683aeecc58300b4a3a834af2baf458ff136f7e
bruce chamblee <brucechamblee@gmail.com>
2019-06-09 17:21:23 -0400

    changed the map and index files to match

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -13,7 +13,7 @@ function success(pos) {
   var crd = pos.coords;
 
   console.log("Your current position is:");
-  console.log(`Latitude : ${crd.latitude}`);
+  // console.log(`Latitude : ${crd.latitude}`);
   console.log(`Longitude: ${crd.longitude}`);
   console.log(`More or less ${crd.accuracy} meters.`);
 }
